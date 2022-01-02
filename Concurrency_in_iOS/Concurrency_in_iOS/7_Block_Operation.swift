868b501b4e031641454e86cbb9d3501bdce54aab
ashislaha <mail2ashislaha@gmail.com>
2022-01-02 09:06:46 +0530

    maintaining block operations order among each other

--- Concurrency_in_iOS/Concurrency_in_iOS/7_Block_Operation.swift
+++ Concurrency_in_iOS/Concurrency_in_iOS/7_Block_Operation.swift
@@ -24,6 +24,10 @@ class TestBlockOperation {
 			}
 		}
 		
+		blockOperaiton.completionBlock = {
+			print("[TestBlockOperation] all tasks are completed.")
+		}
+		
 		let date1 = Date()
 		blockOperaiton.start()
 		let date2 = Date()
@@ -35,6 +39,42 @@ class TestBlockOperation {
 	class func test() {
 		let obj = TestBlockOperation()
 		obj.isConcurrentTesting()
+		
+		obj.orderOperations()
+	}
+	
+	
+	// I am creating a local variable as we are trying to start 2nd operation on completion of 1st operation.
+	private var blockOperation2: BlockOperation?
+	
+	func orderOperations() {
+		let blockOperation1 = BlockOperation {
+			print("[BlockOperation] operation 1: Do multiplication / division first")
+		}
+		
+		blockOperation1.completionBlock = {
+			print("[BlockOperation] operation 1: Completed")
+			//self.blockOperation2?.start()
+		}
+		
+		blockOperation2 = BlockOperation {
+			print("[BlockOperation] operation 2: Do addition second")
+		}
+		
+		blockOperation2?.completionBlock = {
+			print("[BlockOperation] operation 2: Completed")
+		}
+		
+		blockOperation2?.addDependency(blockOperation1)
+		
+		// when to start the operations?
+		// If the block operations are doing sync operation then we can start sequentially.
+		blockOperation1.start()
+		blockOperation2?.start()
+		
+		// If 2nd task is dependent on result of 1st operation then 1st operation is async,
+		// then 2nd task should not start immediately, rather than it should start when 1st task is completed.
+		//blockOperation1.start()
 	}
 	
 }
